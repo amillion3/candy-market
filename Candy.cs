921df60953d66ccc14dd2edc9c36334583885b00
Andy Million <amillion3@gmail.com>
2018-12-23 09:40:31 -0600

    Updated model to include name, manufacturer, flavor category, and date received

--- Candy.cs
+++ Candy.cs
@@ -1,7 +1,12 @@
-﻿namespace candy_market
+﻿using System;
+
+namespace candy_market
 {
     internal class Candy
     {
         public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string flavorCategory { get; set; }
+        public DateTime dateReceived { get; set; }
     }
 }
